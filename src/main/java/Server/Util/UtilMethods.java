9794b788f839bf3fef70e1227c55e12f21650ff3
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2019-01-02 17:33:41 +0530

    add todayDate(), timestamp() methods

--- src/main/java/Server/Util/UtilMethods.java
+++ src/main/java/Server/Util/UtilMethods.java
@@ -1,5 +1,9 @@
 package Server.Util;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 
@@ -13,11 +17,36 @@ public class UtilMethods {
 
     }
 
+
+
+
     public static void generateRandomAmount() throws Exception{
 
         Random r = new Random();
         double randomValue = Integer.parseInt("100") - (Integer.parseInt("100")) * r.nextDouble();
     }
 
+    //YYYY-MM-DD
+    public static String todayDate() throws Exception{
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-DD");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+
+    //YYYY-MM-DD HH:mm:ss.sss
+    public static String timestamp() throws Exception{
+
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        String tString = timestamp.toString();
+        if(tString.length()<23){
+            for (int i= tString.length(); i<23;i++){
+                tString = tString+"0";
+            }
+        }
+        return tString;
+    }
 
 }

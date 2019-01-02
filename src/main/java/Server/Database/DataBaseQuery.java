b07b61dd66a9673bc24ba92d784df3734a8dcbe4
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2019-01-02 17:27:35 +0530

    change database value retrieving and add JSON object to handle the response

--- src/main/java/Server/Database/DataBaseQuery.java
+++ src/main/java/Server/Database/DataBaseQuery.java
@@ -1,7 +1,9 @@
 package Server.Database;
 
-import Server.Util.Response;
 import Server.Util.SessionHandler;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,18 +11,30 @@ import java.sql.ResultSet;
 
 public class DataBaseQuery {
 
-    public static void getCricketGames(SessionHandler s, Response r) throws Exception{
+    public static void getCricketGames(SessionHandler s) throws Exception{
 
         ResultSet rs = null;
         Connection conn = s.getDbCon();
         PreparedStatement ps = null;
 
+        JSONArray jsonArray = new JSONArray();
+
         try {
-            ps = conn.prepareStatement("SELECT * FROM CRICKET_GAMES order by date desc");
+            ps = conn.prepareStatement("SELECT * FROM cricket_games order by Date desc");
             rs = ps.executeQuery();
             while (rs.next()) {
-                System.out.println("Username: " + rs.getString("tech_username"));
+
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("GameCode",rs.getString("GameCode"));
+                jsonObject.put("TeamOne",rs.getString("TeamOne"));
+                jsonObject.put("TeamTwo",rs.getString("TeamTwo"));
+                jsonObject.put("Date",rs.getString("Date"));
+
+                jsonArray.add(jsonObject);
+
             }
+
+            s.setList(jsonArray);
 
         } catch(Exception e) {
             e.printStackTrace();
@@ -33,7 +47,7 @@ public class DataBaseQuery {
     }
 
 
-    public static void getGameDetails(SessionHandler s, Response r) throws Exception{
+    public static void getGameDetails(SessionHandler s) throws Exception{
         ResultSet rs = null;
         Connection conn = s.getDbCon();
         PreparedStatement ps = null;
@@ -45,7 +59,10 @@ public class DataBaseQuery {
 
             rs = ps.executeQuery();
             while (rs.next()) {
-                r.setGameCode(rs.getString("gameCode"));
+//                r.setGameCode(rs.getString("GameCode"));
+//                r.setTeamOne(rs.getString("TeamOne"));
+//                r.setTeamTwo(rs.getString("TeamTwo"));
+//                r.setGameDate(rs.getString("Date"));
             }
 
         } catch(Exception e) {
--- src/main/java/Server/Database/DatabaseLayer.java
+++ src/main/java/Server/Database/DatabaseLayer.java
@@ -1,18 +1,17 @@
 package Server.Database;
 
-import Server.Util.Response;
 import Server.Util.SessionHandler;
 
 public class DatabaseLayer {
 
     //Get Data    ---------------------------------------------------------------
 
-    public static void getCricketGames(SessionHandler s, Response r) throws Exception{
-        DataBaseQuery.getCricketGames(s,r);
+    public static void getCricketGames(SessionHandler s) throws Exception{
+        DataBaseQuery.getCricketGames(s);
     }
 
-    public static void getGameDetails(SessionHandler s, Response r) throws Exception{
-        DataBaseQuery.getGameDetails(s,r);
+    public static void getGameDetails(SessionHandler s) throws Exception{
+        DataBaseQuery.getGameDetails(s);
     }
 
     //Insert    ---------------------------------------------------------------

ce624c0b7dca5e2a6194ad2a9a4fd6cc45993bc4
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2019-01-02 17:20:47 +0530

    Add Log handling and db details changed.

--- src/main/java/Server/Database/DatabaseConfig.java
+++ src/main/java/Server/Database/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package Server.Database;
 
 
+import Server.Util.LogHandler;
 import Server.Util.SessionHandler;
 
 import java.sql.DriverManager;
@@ -10,11 +11,12 @@ import java.sql.SQLException;
 public class DatabaseConfig {
 
     private static String userName   = "root";
-    private static String url        = "jdbc:mysql://localhost:3306/cricket";// with database
-    private static String password   = "root";
+    private static String dbName     = "cricket";
+    private static String url        = "jdbc:mysql://localhost:3306/"+dbName;
+    private static String password   = "mysql";
 
 
-    public static void makeJDBCConnection(SessionHandler s) {
+    public static void makeJDBCConnection(SessionHandler s) throws Exception {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -24,7 +26,14 @@ public class DatabaseConfig {
         }
 
         try {
+
+            LogHandler.writeInfoFile("00000000000000000000000000000000","Starting Database Connection");
+            LogHandler.writeInfoFile("00000000000000000000000000000000","Database: "+dbName);
+            LogHandler.writeInfoFile("00000000000000000000000000000000","User    : "+userName);
+            LogHandler.writeInfoFile("00000000000000000000000000000000","URL     : "+url);
             s.setDbCon(DriverManager.getConnection(url,userName,password));
+
+            LogHandler.writeInfoFile("00000000000000000000000000000000","Database connected successfully");
 
         } catch (SQLException e) {
             e.printStackTrace();

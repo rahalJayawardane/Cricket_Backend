d161eb343e2a3cc1f31b7b662a668a9c6767e0f1
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2019-01-02 17:31:04 +0530

    add log handling part

--- src/main/java/Server/Util/LogHandler.java
+++ src/main/java/Server/Util/LogHandler.java
@@ -1,4 +1,34 @@
 package Server.Util;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class LogHandler {
+
+    public static void createLogFile() throws Exception{
+
+        String fileSeparator = System.getProperty("file.separator");
+        String fileName      = "INFO_"+UtilMethods.todayDate();
+
+        //absolute file name with path
+        String path = "logs"+fileSeparator+fileName+".txt";
+        File file   = new File(path);
+        file.getParentFile().mkdirs();
+
+        if ( !file.exists()) {
+            FileWriter writer = new FileWriter(file);
+        }
+
+    }
+
+    public synchronized static void writeInfoFile(String reqId, String message) throws Exception{
+        FileWriter fw = new FileWriter("logs/INFO_"+UtilMethods.todayDate()+".txt",true);
+        fw.write("\n");
+        fw.write(UtilMethods.timestamp()+" INFO "+reqId+" --- "+message);
+        fw.close();
+
+        System.out.println(UtilMethods.timestamp()+"  INFO "+reqId+" --- "+message);
+    }
+
 }

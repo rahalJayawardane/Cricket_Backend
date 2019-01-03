06d5f98256eb02d09ccf415ebc8cbfec40846e50
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2019-01-03 18:01:07 +0530

    add java file for insert methods

---
+++ src/main/java/Server/Transactions/Insert.java
@@ -0,0 +1,29 @@
+package Server.Transactions;
+
+import Server.Database.DatabaseLayer;
+import Server.Util.LogHandler;
+import Server.Util.Response;
+import Server.Util.SessionHandler;
+import Server.Util.UtilMethods;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Rahal on 1/1/2019.
+ */
+
+@RestController
+
+
+@RequestMapping("/cricket/insert")
+public class Insert {
+
+    String appId = "abc";
+    String intId = "123";
+
+
+
+
+
+
+}

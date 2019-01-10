0075fe14f5ca283e77f37fea3f1275d1ed043e43
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2019-01-10 20:56:35 +0530

    add insertGameDetails

--- src/main/java/Server/Cricket/Transactions/Insert.java
+++ src/main/java/Server/Cricket/Transactions/Insert.java
@@ -1,7 +1,17 @@
 package Server.Cricket.Transactions;
 
+import Server.Database.DatabaseLayer;
+import Server.LogHandler.LogWriter;
+import Server.Util.Request;
+import Server.Util.Response;
+import Server.Util.SessionHandler;
+import Server.Util.UtilMethods;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by Rahal on 1/1/2019.
@@ -17,6 +27,34 @@ public class Insert {
     String intId = "123";
 
 
+    /**
+     * Get get games columns details
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/getColDetails")
+    @ResponseBody
+    public CompletableFuture<Response> insertColumndetails(@RequestBody Request request) throws Exception {
+
+        SessionHandler s = new SessionHandler();
+        s.setRequestId(UtilMethods.getReqId());
+        s.setColNo(request.getColNo());
+        s.setGameId(request.getGameId());
+        s.setColNo(request.getColNo());
+        s.setColDetails(request.getColDetails());
+
+
+        LogWriter.writeInfoFile(s.getRequestId(), "Method Calling : Insert Columns Details - Game ID: "+s.getGameId()+" | Column No: "+s.getColNo());
+        LogWriter.writeInfoFile(s.getRequestId(), "Insert details from database");
+
+//        DatabaseLayer.getColumnDetails(s);
+
+        LogWriter.writeInfoFile(s.getRequestId(), "Send response to frontend");
+
+        Response response = new Response(s);
+        return CompletableFuture.completedFuture(response);
+    }
 
 
 
